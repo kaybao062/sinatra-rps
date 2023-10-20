7e24c3c5b406b0d7b45f2e4715897c7b11848445
kaybao062 <116699898+kaybao062@users.noreply.github.com>
2023-10-20 17:57:51 +0000

    com

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/rock") do
+  get = ['paper', 'scissors', 'rock']
+  @choice = 'rock'
+  @react = get.sample
+  
+    erb(:rock)
+  end
+
+get("/paper") do
+  get = ['paper', 'scissors', 'rock']
+  @choice = 'paper'
+  @react = get.sample
+    
+    erb(:paper)
+  end
+
+get("/scissors") do
+  get = ['paper', 'scissors', 'rock']
+  @choice = 'scissors'
+  @react = get.sample
+  
+    erb(:scissors)
+  end
+  
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:main)
 end
